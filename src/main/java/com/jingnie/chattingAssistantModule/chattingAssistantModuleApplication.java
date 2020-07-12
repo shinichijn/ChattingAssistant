4bfc0d3d4211e7152b86a89a35c2344af8c59d5c
unknown <shinichi_jnie@hotmail.com>
2020-07-11 18:52:23 -0700

    Depoly to Heruko 1

---
+++ src/main/java/com/jingnie/chattingAssistantModule/chattingAssistantModuleApplication.java
@@ -0,0 +1,13 @@
+package com.jingnie.chattingAssistantModule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class chattingAssistantModuleApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(chattingAssistantModuleApplication.class, args);
+	}
+}
---
+++ src/test/java/com/jingnie/chattingAssistantModule/chattingAssistantModuleTests.java
@@ -0,0 +1,16 @@
+package com.jingnie.chattingAssistantModule;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class chattingAssistantModuleTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
